0ee17b761654d29ad9e19c5b39686a13324c2c03
Nzihan <9625615@qq.com>
2026-05-10 12:59:53 +0800

    动漫社网站

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask , render_template
+from flask import Flask , render_template , url_for
 
 app = Flask(__name__)
 
@@ -19,6 +19,17 @@ def activities():
         return render_template('no_activities.html')
     else:
         return render_template('activities.html', activities = activities_list)
+
+@app.route('/gallery')
+def gallery():
+    # 定义照片文件夹的路径
+    image_folder = os.path.join(app.static_folder, 'history_photos')
+    # 获取该文件夹下所有文件的名称，并过滤出常见的图片格式
+    image_files = [f for f in os.listdir(image_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+    # 生成每张图片的完整URL列表
+    image_urls = [url_for('static', filename=f'history_photos/{img}') for img in image_files]
+    # 将生成好的URL列表传递给 gallery.html 模板
+    return render_template('gallery.html', image_urls=image_urls)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 7891))
